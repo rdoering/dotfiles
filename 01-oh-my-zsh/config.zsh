8c84656e50c735166ec10e3b0bc5b85115680fa7
rdoering <rdoering.info@gmail.com>
2015-09-23 03:43:24 +0200

    Add oh-my-zsh as submodule.

--- 01-oh-my-zsh/config.zsh
+++ 01-oh-my-zsh/config.zsh
@@ -5,7 +5,7 @@ export ZSH=$DOTFILES/01-oh-my-zsh/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="jreese"
+ZSH_THEME="robert"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
