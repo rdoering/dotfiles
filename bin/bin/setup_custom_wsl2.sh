b6ace58786d562fbde89c82b9e593630ffea4a44
Robert Döring <robert.doering@proton.me>
2024-02-13 09:33:28 +0100

    Update setup_custom_wsl2.sh
    void usage of fish shell

--- bin/bin/setup_custom_wsl2.sh
+++ bin/bin/setup_custom_wsl2.sh
@@ -8,7 +8,7 @@ sudo apt-add-repository --yes --no-update ppa:fish-shell/release-3
 
 # update and install once
 sudo apt update --ignore-missing # some repos lurking behind VPN walls
-sudo apt install --yes --quiet --ignore-missing --fix-broken fish stow fzf fd-find fdclone
+sudo apt install --yes --quiet --ignore-missing --fix-broken fish stow fzf fd-find fdclone rclone restic zsh
 
 echo -n "get my dotfiles ... "
 if [[ -d ~/.dotfiles ]]
@@ -22,17 +22,5 @@ fi
 
 cd ~/.dotfiles && stow */
 
-echo -n "enable fish shell ... "
-PROFILE_CONTENT=$(cat ~/.profile)
-if [[ "$PROFILE_CONTENT" =~ "fish" ]]
-then
-    echo "did before"
-else
-    echo "" >> ~/.profile
-    echo "fish" >> ~/.profile
-    echo "done"
-fi
-
 trap - EXIT
 
-fish
