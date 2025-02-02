543a4ea877ff3578638b035ce7c856da4594c2e6
rdoering <rdoering.info@gmail.com>
2025-02-02 04:40:40 +0100

    source bashrc to verify installation

--- dot_local/bin/executable_install_my_tools.sh
+++ dot_local/bin/executable_install_my_tools.sh
@@ -7,6 +7,7 @@ PACKAGES=(
 )
 
 
+source "$HOME/.bashrc"  # make sure a fresh installed brew could be used
 command -v brew &>/dev/null || { echo "Failed, run install_brew.sh to install Homebrew first"; exit 1; }
 brew install "${PACKAGES[@]}"
 
